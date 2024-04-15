00179f6a18731b2714426ca72fb77b6631e76b4c
Muttant <zangiriimochi@gmail.com>
2024-04-16 01:00:53 +0700

    Initial commit

---
+++ src/main/java/com/tutorial/ecommerce_be/EcommerceBeApplication.java
@@ -0,0 +1,13 @@
+package com.tutorial.ecommerce_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceBeApplication.class, args);
+	}
+
+}
